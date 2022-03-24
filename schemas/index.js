81881c144d33a2bafe50e45edc45f8e93c14d180
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-03-24 12:16:43 +0900

    task

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect("mongodb://localhost:27017/hanghaeblog", { ignoreUndefined: true })
+    .connect("mongodb://localhost:27017/task", { ignoreUndefined: true })
     .catch((err) => {
       console.error(err);
     });
